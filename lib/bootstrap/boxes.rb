b85ebdc37915b2924385374c7d7763ccf293264d
wuwx <wuweixin@gmail.com>
2013-03-22 15:07:18 +0800

    bootstrap-boxes gem

---
+++ lib/bootstrap/boxes.rb
@@ -0,0 +1,7 @@
+require "bootstrap/boxes/version"
+
+module Bootstrap
+  module Boxes
+    # Your code goes here...
+  end
+end
---
+++ lib/bootstrap/boxes/version.rb
@@ -0,0 +1,5 @@
+module Bootstrap
+  module Boxes
+    VERSION = "0.0.1"
+  end
+end
